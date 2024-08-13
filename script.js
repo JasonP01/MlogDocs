7c63f1095ec4fd2d3a7ff9a2b82c4ef240b453df
Yrueii <aterlaze@gmail.com>
2024-08-13 15:14:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('hamburger-menu').addEventListener('click', function() {
+    var sidebar = document.getElementById('sidebar');
+    if (sidebar.classList.contains('open')) {
+        sidebar.classList.remove('open');
+    } else {
+        sidebar.classList.add('open');
+    }
+});
