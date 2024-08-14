288ca7ccab99bb9d6f5cd0395a18be3a6a3f7221
Yrueii <aterlaze@gmail.com>
2024-08-14 11:11:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 document.getElementById('hamburger-menu').addEventListener('click', function() {
     var sidebar = document.getElementById('sidebar');
+    var content = document.querySelector('.main-content');
+    
     if (sidebar.classList.contains('open')) {
         sidebar.classList.remove('open');
+        content.classList.remove('open');
     } else {
         sidebar.classList.add('open');
+        content.classList.add('open');
     }
+    
+}); 
+
+
+document.querySelectorAll('.sidebar-link').forEach(function(link) {
+    link.addEventListener('click', function() {
+        var sidebar = document.getElementById('sidebar');
+        var content = document.querySelector('.main-content');
+        
+        if (sidebar.classList.contains('open')) {
+            sidebar.classList.remove('open');
+            content.classList.remove('open');
+        }
+    });
 });
+
+
