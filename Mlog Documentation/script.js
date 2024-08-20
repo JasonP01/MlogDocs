cbbab8e861d30e3c5b3e88fa55ef247a190b9c08
Yrueii <aterlaze@gmail.com>
2024-08-20 11:16:23 +0700

    displays an error message when image is not loaded

--- Mlog Documentation/script.js
+++ Mlog Documentation/script.js
@@ -25,4 +25,44 @@ document.querySelectorAll('.sidebar-link').forEach(function(link) {
     });
 });
 
+document.getElementById('hamburger-menu').addEventListener('click', function() {
+    var sidebar = document.getElementById('sidebar');
+    var content = document.querySelector('.main-content');
+    
+    if (sidebar.classList.contains('open')) {
+        sidebar.classList.remove('open');
+        content.classList.remove('open');
+    } else {
+        sidebar.classList.add('open');
+        content.classList.add('open');
+    }
+    
+}); 
+
+
+document.querySelectorAll('.sidebar-link').forEach(function(link) {
+    link.addEventListener('click', function() {
+        var sidebar = document.getElementById('sidebar');
+        var content = document.querySelector('.main-content');
+        
+        if (sidebar.classList.contains('open')) {
+            sidebar.classList.remove('open');
+            content.classList.remove('open');
+        }
+    });
+});
+
+document.querySelectorAll('img').forEach(img => {
+    img.addEventListener('error', function() {
+        this.style.display = 'none';
+
+        let errorMessage = document.createElement('span');
+        errorMessage.className = 'error-message';
+        errorMessage.textContent = 'Image loading error, try reloading the page';
+        
+        this.parentNode.insertBefore(errorMessage, this.nextSibling);
+        errorMessage.style.display = 'block';
+    });
+});
+
 
