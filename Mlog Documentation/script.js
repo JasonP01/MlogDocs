9570dbcd9113adcbf3701bd5f0d488989a2018a2
Yrueii <aterlaze@gmail.com>
2024-08-20 11:17:19 +0700

    display error when an image is not loaded

--- Mlog Documentation/script.js
+++ Mlog Documentation/script.js
@@ -25,33 +25,6 @@ document.querySelectorAll('.sidebar-link').forEach(function(link) {
     });
 });
 
-document.getElementById('hamburger-menu').addEventListener('click', function() {
-    var sidebar = document.getElementById('sidebar');
-    var content = document.querySelector('.main-content');
-    
-    if (sidebar.classList.contains('open')) {
-        sidebar.classList.remove('open');
-        content.classList.remove('open');
-    } else {
-        sidebar.classList.add('open');
-        content.classList.add('open');
-    }
-    
-}); 
-
-
-document.querySelectorAll('.sidebar-link').forEach(function(link) {
-    link.addEventListener('click', function() {
-        var sidebar = document.getElementById('sidebar');
-        var content = document.querySelector('.main-content');
-        
-        if (sidebar.classList.contains('open')) {
-            sidebar.classList.remove('open');
-            content.classList.remove('open');
-        }
-    });
-});
-
 document.querySelectorAll('img').forEach(img => {
     img.addEventListener('error', function() {
         this.style.display = 'none';
@@ -64,5 +37,3 @@ document.querySelectorAll('img').forEach(img => {
         errorMessage.style.display = 'block';
     });
 });
-
-
