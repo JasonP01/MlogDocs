0f590df7e9309c8dc69554d86a15d695b5a3d31d
Yruei <aterlaze@gmail.com>
2024-10-14 10:26:57 +0700

    update labeler

--- Mlog Documentation/MlogLabeler/script.js
+++ Mlog Documentation/MlogLabeler/script.js
@@ -1,7 +1,8 @@
 
 
-
-
+let indent = true;
+let timeout;
+let timeout1;
 function submitText() {
     const mcode = document.getElementById("input").value
     if (mcode) {
@@ -24,12 +25,20 @@ function submitText() {
                 lmcode += "j" + String(index) + ":" + "\n";
             }
             if (firstWord === "jump" && !isNaN(words[1])) {
-                lmcode +=`    ${words[0]} j${words[1]} ${words.slice(2).join(" ")}\n`
+                if (indent == false) {
+                    lmcode +=`${words[0]} j${words[1]} ${words.slice(2).join(" ")}\n`
+                } else {
+                    lmcode +=`    ${words[0]} j${words[1]} ${words.slice(2).join(" ")}\n`
+                }
+                
             } else {
                 // lmcode += "    " + (words.join(" ")) + "\n";
-                lmcode += `    ${words.join(" ")}\n`
+                if (indent == false) {
+                    lmcode += `${words.join(" ")}\n`
+                } else {
+                    lmcode += `    ${words.join(" ")}\n`
+                }
             }
-
         });
     const output = document.getElementById("output");
     output.value = lmcode
@@ -37,6 +46,46 @@ function submitText() {
 }
 
 function copy() {
-    const lmcode = output.get("1.0", "end");
-    pyperclip.copy(lmcode);
+    let copyText = document.getElementById("output").value;
+    statusElement = document.getElementById("status");
+    statusElement.style.transition = "none";
+    statusElement.style.opacity = "1";
+    navigator.clipboard.writeText(copyText).then(() => {
+        statusElement.textContent = "Text copied to clipboard!";
+      }).catch(err => {
+        statusElement.textContent= "Failed to copy text.";
+        console.error('Error copying text: ', err);
+      });
+
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+
+      if (timeout1) {
+        clearTimeout(timeout1);
+      }
+
+      timeout = setTimeout(() => {
+        statusElement.style.transition = "opacity 1.5s ease";
+        statusElement.style.opacity = "0";
+        timeout1 = setTimeout(() => {
+            statusElement.textContent= "";
+        }, 1500);
+      }, 1000);
+    // copyText.select();
+    // copyText.setSelectionRange(0, 99999);
+    // document.execCommand("copy");
+    // document.getElementById("status").textContent = "Text copied to clipboard!";
+    }
+    
+
+function toggleButton() {
+    indent = !indent;
+    
+    const button = document.getElementById("toggleButton");
+    button.className = indent ? "toggled" : "untoggled";
+
+
 }
+
+
